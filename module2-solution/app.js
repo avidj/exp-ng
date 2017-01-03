f59e352b54b5d41cea8fa804e70ba80b4b13bc63
David Kensche <david.kensche@gmail.com>
2017-01-03 21:39:56 +0100

    solution 2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -13,17 +13,59 @@
 
     Config.$inject = [ 'ShoppingListServiceProvider' ];
     function Config(ShoppingListServiceProvider) {
-
     }
 
     ToBuyList.$inject = [ 'ShoppingListService' ];
     function ToBuyList(ShoppingListService) {
-
+        var toBuyList = this;
+        toBuyList.items = ShoppingListService.getToBuyList();
+        toBuyList.buy = function(index) {
+            ShoppingListService.buy(index);
+        }
     }
 
     AlreadyBoughtList.$inject = [ 'ShoppingListService' ];
     function AlreadyBoughtList(ShoppingListService) {
+        var alreadyBoughtList = this;
+        alreadyBoughtList.items = ShoppingListService.getBoughtList();
+    }
 
+    function ShoppingListService() {
+        var service = this;
+        var toBuyList = [
+            {
+                name: "Cookies",
+                quantity: 10
+            },
+            {
+                name: "Chocolate",
+                quantity: 5
+            },
+            {
+                name: "Salad",
+                quantity: 2
+            },
+            {
+                name: "Beer",
+                quantity: 10
+            },
+            {
+                name: "Cheese",
+                quantity: 2
+            }
+        ];
+        var boughtList = [];
+        service.getToBuyList = function() {
+            return toBuyList;
+        };
+        service.getBoughtList = function() {
+            return boughtList;
+        }
+        service.buy = function(index) {
+            var item = toBuyList[index];
+            boughtList.push(item);
+            toBuyList.splice(index, 1);
+        };
     }
 
     function ShoppingListServiceProvider() {

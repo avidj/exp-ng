d4bc4d3cf7396c6b37c7277ee94fe9e1e534d273
David Kensche <david.kensche@gmail.com>
2017-01-14 22:19:49 +0100

    filter in the service

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -46,17 +46,10 @@
         menu.items = [];
 
         menu.narrowItDown = function() {
-            var menuItemsPromise = MenuSearchService.getMenuItems();
+            var menuItemsPromise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
             menuItemsPromise
                 .then(function(response) {
-                    var items = response.data.menu_items;
-                    var searchTerm = menu.searchTerm.toLowerCase();
-                    menu.found = [];
-                    for ( var i = 0; i < items.length; i++ ) {
-                        if ( items[i].description.toLowerCase().indexOf(searchTerm) !== -1 ) {
-                            menu.found.push(items[i]);
-                        }
-                    }
+                    menu.found = response;
                 })
                 .catch(function(error) {
                     console.log(error);
@@ -68,42 +61,21 @@
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
 
-        service.getMenuItems = function() {
-            var response = $http(
-                { url: ( ApiBasePath + '/menu_items.json' ) }
-            );
-            return response;
-        };
-    }
-/*
-    MenuSearchService.$inject = [ '$q', '$http', 'ApiBasePath' ];
-    function MenuSearchService($q, $http, ApiBasePath) {
-        var service = this;
-
         service.getMatchedMenuItems = function(searchTerm) {
-            var deferred = $q.defer();
+            searchTerm = searchTerm.toLowerCase();
             var menuItemsPromise = service.getMenuItems();
-            menuItemsPromise
-                .then(
-                    function success(response) {
-                        searchTerm = searchTerm.toLowerCase();
-                        var items = response.data.menu_items;
-                        var found = [];
-                        for ( var i = 0; i < items.length; i++ ) {
-                            if ( items[i].description.toLowerCase().indexOf(searchTerm) !== -1 ) {
-                                found.push(items[i]);
-                            }
+            return menuItemsPromise
+                .then(function(response) {
+                    var items = response.data.menu_items;
+                    var found = [];
+                    for ( var i = 0; i < items.length; i++ ) {
+                        if ( items[i].description.toLowerCase().indexOf(searchTerm) !== -1 ) {
+                            found.push(items[i]);
                         }
-                        deferred.resolve(found); // the then-function returns a promise wrapping this
-                    },
-                    function error(response) {
-                        console.log(error);
-                    })
-                .catch(function(error) {
-                    console.log(error);
+                    }
+                    return found; // the then-function returns a promise wrapping this
                 });
-            return deferred.promise;
-        }
+        };
 
         service.getMenuItems = function() {
             var response = $http(
@@ -112,5 +84,4 @@
             return response;
         };
     }
-*/
 })();
